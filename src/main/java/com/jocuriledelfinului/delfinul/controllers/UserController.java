aa793784090faa2e3ff9a7d2e0f9c9d4453f9a1f
vladirares <jeleavladimir@yahoo.ro>
2021-11-13 18:53:00 +0200

    generateBoard

--- src/main/java/com/jocuriledelfinului/delfinul/controllers/UserController.java
+++ src/main/java/com/jocuriledelfinului/delfinul/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.jocuriledelfinului.delfinul.controllers;
 import com.jocuriledelfinului.delfinul.enums.Terrain;
 import com.jocuriledelfinului.delfinul.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,12 @@ import reactor.core.publisher.Mono;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @RequestMapping("/public/")
 @RestController
+@CrossOrigin
 public class UserController {
 
     @Autowired
@@ -22,6 +25,8 @@ public class UserController {
     @GetMapping("generate/board")
     public Mono<List<List<Terrain>>> generateBoard(@PathParam("size") Integer size, @PathParam("strategy") String strategy) {
         Logger.getLogger(UserController.class.getName()).info("generated board ");
-        return userService.generateBoard(size, strategy);
+        return Optional.ofNullable(strategy)
+                .map(strat -> userService.generateBoard(size,strat))
+                .orElse(userService.generateBoard(size,""));
     }
 }
