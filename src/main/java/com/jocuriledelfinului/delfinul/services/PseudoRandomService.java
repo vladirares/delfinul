6af25b56ad89c7f2852da4fe8c860013f346a592
vladirares <jeleavladimir@yahoo.ro>
2022-01-23 12:05:16 +0200

    all functionality for PseudoRandom

---
+++ src/main/java/com/jocuriledelfinului/delfinul/services/PseudoRandomService.java
@@ -0,0 +1,22 @@
+package com.jocuriledelfinului.delfinul.services;
+
+import com.jocuriledelfinului.delfinul.enums.Terrain;
+import com.jocuriledelfinului.delfinul.strategies.Strategy;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class PseudoRandomService {
+    private static final Random random = new Random();
+
+    public static int getNextUnsigned(int upperBound) {
+        return random.nextInt(upperBound);
+    }
+
+    public static Terrain getNextStandardTerrain(Strategy strategy) {
+        return strategy.STANDARD_TILE_SET.stream()
+                .skip(random.nextInt(strategy.STANDARD_TILE_SET.size()))
+                .findFirst().get();
+    }
+}
